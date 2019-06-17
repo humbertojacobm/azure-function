f23145ef838c67f84a6ca86b69b2e7fa92131b1e
Humberto Mori <hmori@brainshark.com>
2019-06-17 23:51:25 +0000

    generating file in blob storage and sending email

---
+++ GenerateLicenseFile.cs
@@ -0,0 +1,28 @@
+using System;
+using System.IO;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace pluralsightfuncs
+{
+    public static class GenerateLicenseFile
+    {
+        [FunctionName("GenerateLicenseFile")]
+        public static void Run(
+            [QueueTrigger("orders", Connection = "AzureWebJobsStorage")]Order order, 
+            [Blob("licenses/{rand-guid}.lic")] TextWriter ouputBlob,
+            ILogger log)
+        {
+            log.LogInformation($"C# Queue trigger function processed: {order}");
+            ouputBlob.WriteLine($"orderId: {order.OriderId}");
+            ouputBlob.WriteLine($"Email: {order.Email}");
+            ouputBlob.WriteLine($"ProductId: {order.ProductId}");
+            ouputBlob.WriteLine($"PurchaseDate: {DateTime.UtcNow}");
+            var md5 = System.Security.Cryptography.MD5.Create();
+            var hash = md5.ComputeHash(
+                  System.Text.Encoding.UTF8.GetBytes(order.Email + "secret"));
+            ouputBlob.WriteLine($"Secreate code : {BitConverter.ToString(hash).Replace("-", "")}");
+        }
+    }
+}
